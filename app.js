1a3a72db5492463f91b13da8146ecac07f7525b2
Grace Liu <graceliu@Graces-Air.home>
2021-11-21 10:48:05 -0500

    add project and profile questions

--- app.js
+++ app.js
@@ -7,13 +7,84 @@ const inquirer = require ('inquirer')
 
 //   console.log('portfolio complete! check out index.html to see the output')
 // })
-
-inquirer
-  .prompt([
+const promptUser =() =>{
+return inquirer.prompt([
     {
       type:'input',
       name: 'name',
       message:'What is your name?'
+    },
+    {
+      type:'input',
+      name:'github',
+      message:'Enter your GitHub Username: '
+    },
+    {
+    type:'input',
+    name:'about',
+    message:'Provide some information about yourself: '
+    }
+  ]);
+}
+ 
+const promptProject =portfolioData =>{
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+   //if there is no 'projects' array property, create one!
+   if(!portfolioData.projects){
+    portfolioData.projects=[];
+    }
+    
+  return inquirer.prompt([
+    {
+      type:'input',
+      name:'name',
+      message:'What is the name of your project?'
+    },
+    {
+      type:'input',
+      name:'description',
+      message:'Provide a description of the project (Required).'
+    },
+    {
+      type:'checkbox',
+      name:'language',
+      message:'What did you build this project with? (check all that apply)',
+      choices:['JavaScript','HTML','CSS','ES6','jQuery','Bootstrap','Node']
+    },
+    {
+      type:'input',
+      name:'link',
+      message:'Enter the GitHub link to you project. (Required)'
+    },
+    {
+      type:'confirm',
+      name:'feature',
+      message:'Would you like to feature this project?',
+      default:false
+    },
+    {
+      type:'confirm',
+      name:'confirmAddProject',
+      message:'Would you like to enter another project?',
+      default:false
     }
   ])
-    .then(answer => console.log(answer))
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if(projectData.confirmAddProject){
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
+
+promptUser()
+.then(promptProject)
+  .then(portfolioData =>{
+    console.log(portfolioData)
+  });
